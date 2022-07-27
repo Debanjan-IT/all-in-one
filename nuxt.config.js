0bf6c8149a6e220fd227d424e30b301785a38eee
Debanjan Dasgupta <69705770+Debanjan-IT@users.noreply.github.com>
2022-07-28 02:16:10 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  server: {
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
 }
